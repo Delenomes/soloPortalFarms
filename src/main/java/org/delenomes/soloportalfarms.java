ad707f88571aa60c6ea272d0196e48ad35e4d418
Delenomes <130280621+Delenomes@users.noreply.github.com>
2023-12-30 12:25:55 +0300

    Add files via upload
    Configuration update is here!

--- src/main/java/org/delenomes/soloportalfarms.java
+++ src/main/java/org/delenomes/soloportalfarms.java
@@ -1,11 +1,13 @@
 package org.delenomes;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPortalEnterEvent;
-import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.LivingEntity;
+
+import java.util.List;
 
 
 public final class soloportalfarms extends JavaPlugin implements Listener {
@@ -14,6 +16,7 @@ public final class soloportalfarms extends JavaPlugin implements Listener {
     public void onEnable() {
         System.out.println("Plugin is running!");
         getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
 
     }
 
@@ -21,7 +24,21 @@ public final class soloportalfarms extends JavaPlugin implements Listener {
     public void onEntityPortalEntry(EntityPortalEnterEvent event){
         if (event.getEntity() instanceof LivingEntity) {
             LivingEntity living = (LivingEntity) event.getEntity();
-            living.setRemoveWhenFarAway(false);
+            if (getConfig().getBoolean("enable-blacklist")){
+                List<String> mobs = getConfig().getStringList("blacklisted-mobs");
+                for (String mob : mobs){
+
+                    if (event.getEntityType() == EntityType.valueOf(mob)){
+                        return;
+                    }
+                    else{
+                        living.setRemoveWhenFarAway(false);
+                    }
+                }
+            }
+            else {
+                living.setRemoveWhenFarAway(false);
+            }
 
         }
 
